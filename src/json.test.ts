3582f65fa85bbfb2dc8f82b50a46c0729c09080a
asgervelling <asger.skov.velling@gmail.com>
2024-07-08 12:42:16 +0200

    First commit

---
+++ src/json.test.ts
@@ -0,0 +1,87 @@
+import { describe, expect, test } from "@jest/globals";
+import {
+  escapedQuote,
+  orEmptyString,
+  parseString,
+  whitespaceSurrounded,
+} from "./json";
+import { anyChar, ResultType, str } from "arcsecond";
+
+function asSuccess<T, E, D>(parseResult: ResultType<T, E, D>) {
+  if (!parseResult.isError) {
+    return parseResult;
+  } else {
+    console.log("Failing:");
+    console.log(parseResult);
+    const error = parseResult.error as string;
+    throw new Error(error);
+  }
+}
+
+describe("escapedQuote", () => {
+  test("successfully parses escaped double quotes", () => {
+    const result = escapedQuote.run(`\\"`);
+    expect(asSuccess(result).result).toEqual('\\"');
+  });
+  test("fails to parse unescaped double quotes", () => {
+    const result = escapedQuote.run(`"`);
+    expect(result.isError).toBe(true);
+  });
+  test("successfully parses escaped single quotes", () => {
+    const result = escapedQuote.run(`\\'`);
+    expect(asSuccess(result).result).toEqual("\\'");
+  });
+  test("fails to parse unescaped single quotes", () => {
+    const result = escapedQuote.run(`'`);
+    expect(result.isError).toBe(true);
+  });
+});
+
+describe("parseString", () => {
+  test("successfully parses a string", () => {
+    const result = parseString.run(`"hello"`);
+    expect(asSuccess(result).result).toEqual("hello");
+  });
+  test("successfully parses a string with escaped double quotes", () => {
+    const result = parseString.run(`"hello \\"world\\""`);
+    expect(asSuccess(result).result).toEqual(`hello \\"world\\"`);
+  });
+  test("successfully parses a string with escaped single quotes", () => {
+    const result = parseString.run(`"hello \\'world\\'"`);
+    expect(asSuccess(result).result).toEqual(`hello \\'world\\'`);
+  });
+  test("fails to parse a string without closing double quotes", () => {
+    const result = parseString.run(`"hello`);
+    expect(result.isError).toBe(true);
+  });
+  test("fails to parse a string without opening double quotes", () => {
+    const result = parseString.run(`hello"`);
+    expect(result.isError).toBe(true);
+  });
+});
+
+describe("orEmptyString", () => {
+  test("successfully parses a string", () => {
+    const result = orEmptyString(parseString).run(`"hello"`);
+    expect(asSuccess(result).result).toEqual("hello");
+  });
+  test("successfully parses an empty string", () => {
+    const result = orEmptyString(parseString).run(``);
+    expect(asSuccess(result).result).toEqual("");
+  });
+});
+
+describe("whiteSpaceSurrounded", () => {
+  test("successfully parses a string with whitespace", () => {
+    const result = whitespaceSurrounded(parseString).run(`   "hello"   `);
+    expect(asSuccess(result).result).toEqual("hello");
+  });
+  test("successfully parses an empty string with whitespace", () => {
+    const result = whitespaceSurrounded(str("d")).run(`  d `);
+    expect(asSuccess(result).result).toEqual("d");
+  });
+  test("does not fail when there is no whitespace", () => {
+    const result = whitespaceSurrounded(parseString).run(`"hello"`);
+    expect(asSuccess(result).result).toEqual("hello");
+  });
+});
---
+++ src/json.ts
@@ -0,0 +1,33 @@
+import {
+  char,
+  sequenceOf,
+  str,
+  choice,
+  possibly,
+  sepBy,
+  many,
+  anythingExcept,
+  digits,
+  optionalWhitespace,
+  between,
+  anyOfString,
+  recursiveParser,
+  anyCharExcept,
+  Parser,
+} from "arcsecond";
+
+export const escapedQuote = sequenceOf([str("\\"), anyOfString(`"'`)]).map(
+  (x) => x.join("")
+);
+
+export const parseString = sequenceOf([
+  char('"'),
+  many(choice([escapedQuote, anyCharExcept(char('"'))])).map((x) => x.join("")),
+  char('"'),
+]).map(([_, result]) => result);
+
+export const orEmptyString = (parser: Parser<string, string, any>) =>
+  possibly(parser).map((x) => (x ? x : ""));
+
+export const whitespaceSurrounded =
+  between(optionalWhitespace)(optionalWhitespace);
